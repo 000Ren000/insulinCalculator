f3ed941bd85cc14607014524569ac2e74853260d
Антон <antonren@yandex.ru>
2023-07-30 17:53:57 +0700

    Initial Commit

---
+++ Insulin Calculator/Insulin_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insulin_CalculatorApp.swift
+//  Insulin Calculator
+//
+//  Created by Антон Фондов on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Insulin_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
